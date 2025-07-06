a789a8d05f2bf5d6088c31fe3f51f3ed1ccd46eb
derick061020@gmail.com <derick061020@gmail.com>
2025-07-06 16:09:20 -0500

    Whatsapp

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -61,6 +61,7 @@ class WebhookController extends Controller
 
 
             // Marcar mensaje como leído
+            $business_phone_number_id = $request->json('entry.0.changes.0.value.metadata.phone_number_id');
             Http::withHeaders([
                 'Authorization' => 'Bearer ' . self::GRAPH_API_TOKEN,
             ])->post("https://graph.facebook.com/v22.0/{$business_phone_number_id}/messages",[
