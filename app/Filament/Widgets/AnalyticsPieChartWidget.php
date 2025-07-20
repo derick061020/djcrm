88b19afcd6b487d0e67f88d57b12fc1d85b5c988
derick061020@gmail.com <derick061020@gmail.com>
2025-07-20 10:51:13 -0500

    agregando cambios

--- app/Filament/Widgets/AnalyticsPieChartWidget.php
+++ app/Filament/Widgets/AnalyticsPieChartWidget.php
@@ -72,7 +72,7 @@ class AnalyticsPieChartWidget extends ChartWidget
                         0,
                         0,
                         0,
-                        0,d
+                        0,
                     ],
                     'backgroundColor' => [
                         '#4F46E5', // Indigo
