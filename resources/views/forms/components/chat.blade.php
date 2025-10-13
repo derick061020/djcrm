39b985caf3b52aa18bff4c939eaa9ba0c25a71f4
derick061020@gmail.com <derick061020@gmail.com>
2025-10-13 11:06:03 -0500

    hola

--- resources/views/forms/components/chat.blade.php
+++ resources/views/forms/components/chat.blade.php
@@ -184,7 +184,7 @@ use App\Models\Whatsapp;
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
                         <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clip-rule="evenodd" />
                     </svg>
-                    <span x-text="'fileName' || 'Archivo'" class="text-sm text-gray-700"></span>
+                    <span x-text="'Enviando ...' || 'Archivo'" class="text-sm text-gray-700"></span>
                     <span wire:loading wire:target="file" class="ml-2">
                         <svg class="animate-spin h-4 w-4 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
